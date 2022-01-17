1b7ff1da8ee4f89fe08e56e4f16aaa547ddb8cd4
Hannah Dong <hannah.dong@lincolnprivatejets.com>
2022-01-17 14:05:26 -0500

    loading animation

--- app.js
+++ app.js
@@ -44,27 +44,3 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-
-//image gallery modals (when image is clicked, the full sized image will appear)
-const modal = document.querySelector(".modal");
-const previews = document.querySelectorAll(".gallery-item .image img");
-const original = document.querySelector(".full-img");
-const caption = document.querySelector(".caption");
-
-previews.forEach(preview => {
-    preview.addEventListener('click', () => {
-        modal.classList.add("open");
-        //dynamic change text and image 
-        const originalSrc = preview.getAttribute('data-original');
-        original.src = `./img/photography/${originalSrc}`;
-        const altText = preview.alt;
-        caption.textContent = altText;
-
-    })
-})
-
-modal.addEventListener('click', (e) => {
-    if (e.target.classList.contains("modal")) {
-        modal.classList.remove("open");
-    }
-})
